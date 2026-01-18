965d79bb2462932b8ebd9a84639cc822138c4948
Derek Verduijn <derek.verduijn@proton.me>
2026-01-18 13:23:02 +0100

    getting ready for error routing rewrite

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { isObjKey, loadRoutes, returnStatus } from "./utils.ts";
 
 const routes = await loadRoutes("./routes");
 
-async function handler(req: Request): Promise<Response> {
+Deno.serve({ port: 7776 }, async function (req: Request): Promise<Response> {
   const match = routes.find(([url]) => url.exec(req.url));
   if (!match) return returnStatus(404);
 
@@ -16,6 +16,4 @@ async function handler(req: Request): Promise<Response> {
   ) return await matchRoute[reqMethodUppercase]?.(req, matchPatternResult);
 
   return returnStatus(405);
-}
-
-Deno.serve({ port: 7776 }, handler);
+});
